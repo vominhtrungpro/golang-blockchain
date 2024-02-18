1ba90ac1488ba8740b285cf8528bc7beb842c180
Vo Trung <vominhtrungpro@gmail.com>
2024-02-18 10:28:03 +0700

    with database badger

--- blockchain/block.go
+++ blockchain/block.go
@@ -2,14 +2,10 @@ package blockchain
 
 import (
 	"bytes"
-	"crypto/sha256"
+	"encoding/gob"
+	"log"
 )
 
-type BlockChain struct {
-	Blocks []*Block
-}
-
-// block class
 type Block struct {
 	Hash     []byte
 	Data     []byte
@@ -17,39 +13,46 @@ type Block struct {
 	Nonce    int
 }
 
-// create hash based on previous hash and data
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
-
-// func create actual block from data and previous hash
 func CreateBlock(data string, prevHash []byte) *Block {
 	block := &Block{[]byte{}, []byte(data), prevHash, 0}
-	//block.DeriveHash()
 	pow := NewProof(block)
-
 	nonce, hash := pow.Run()
-	block.Hash = hash
+
+	block.Hash = hash[:]
 	block.Nonce = nonce
 
 	return block
 }
 
-// func to add a block to the chain
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	newBlock := CreateBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, newBlock)
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
 }
 
-// generate a genesis block with first block data
-func Genesis(fbData string) *Block {
-	return CreateBlock(fbData, []byte{})
+func (b *Block) Serialize() []byte {
+	var res bytes.Buffer
+	encoder := gob.NewEncoder(&res)
+
+	err := encoder.Encode(b)
+
+	Handle(err)
+
+	return res.Bytes()
 }
 
-// initialize block chain with first block data
-func InitBlockChain(fbData string) *BlockChain {
-	return &BlockChain{[]*Block{Genesis(fbData)}}
+func Deserialize(data []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+
+	err := decoder.Decode(&block)
+
+	Handle(err)
+
+	return &block
+}
+
+func Handle(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
